181e45d16821634515919af4e092827078cabad5
Sean Kang <sean.kkang@gmail.com>
2020-10-23 13:14:44 +0900

    Update monthly-report.sql

--- programmers/week2/monthly-report.sql
+++ programmers/week2/monthly-report.sql
@@ -2,5 +2,5 @@ SELECT to_char(ts, 'YYYY-MM') AS month, COUNT(DISTINCT userid) as count_active_u
 FROM raw_data.user_session_channel AS A
 JOIN raw_data.session_timestamp AS B
 ON A.sessionid = B.sessionid
-GROUP BY to_char(ts, 'YYYY-MM')
+GROUP BY month
 ORDER BY month DESC
